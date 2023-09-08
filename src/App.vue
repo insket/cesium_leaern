7373edf6394e9615db9b9da90d7c92ef67145668
insket <2694183181@qq.com>
2023-09-08 10:37:33 +0800

    fear: first cesium

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="cesiumContainer"></div>
+</template>
+
+<script setup>
+import * as Cesium from "cesium";
+import { onMounted } from "vue";
+
+onMounted(() => {
+  const viewer = new Cesium.Viewer("cesiumContainer");
+});
+</script>
+
+<style scoped></style>
+
+<style>
+html,
+body,
+#app #cesiumContainer {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+</style>

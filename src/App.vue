17e117830ea89b5f93ba03ff768402bb3c34ee0c
insket <2694183181@qq.com>
2023-09-21 19:18:41 +0800

    style: fullscreen

--- src/App.vue
+++ src/App.vue
@@ -7,22 +7,14 @@ import * as Cesium from "cesium";
 import { onMounted } from "vue";
 
 onMounted(() => {
-  Cesium.Ion.defaultAccessToken =import.meta.env.VITE_APP_ION_KEY
+  Cesium.Ion.defaultAccessToken = import.meta.env.VITE_APP_ION_KEY
 
-  const widget = new Cesium.CesiumWidget("cesiumContainer");
+  new Cesium.Viewer("cesiumContainer");
 });
 </script>
 
 <style scoped></style>
 
 <style>
-html,
-body,
-#app #cesiumContainer {
-  width: 100%;
-  height: 100%;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-}
+
 </style>
--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import cesium from 'vite-plugin-cesium';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),cesium()],
+  server:{
+    port:3000
+  }
 })

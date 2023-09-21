24785819e18fba13a93212aca538f881b5a33862
insket <2694183181@qq.com>
2023-09-21 19:50:25 +0800

    feat:三维地形

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,19 @@ import * as Cesium from "cesium";
 import { onMounted } from "vue";
 
 onMounted(() => {
-  Cesium.Ion.defaultAccessToken = import.meta.env.VITE_APP_ION_KEY
+  Cesium.Ion.defaultAccessToken = import.meta.env.VITE_APP_ION_KEY;
 
-  new Cesium.Viewer("cesiumContainer");
+  const esri = new Cesium.ArcGisMapServerImageryProvider({
+    url: "https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/Mapserver",
+    enablepickFeatures: false,
+  });
+
+  const viewer = new Cesium.Viewer("cesiumContainer", {
+    baseLayerPicker: false,
+    imageryProviderViewModels: [esri], // 自定义图层  要把 baseLayerPicker 设为false
+    terrain: Cesium.Terrain.fromWorldTerrain(), // 三维地形
+  });
 });
 </script>
 
 <style scoped></style>
-
-<style>
-
-</style>

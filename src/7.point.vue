caf5076ddd18f8765a7d161d1b25f7383d506132
insket <2694183181@qq.com>
2023-09-22 22:01:20 +0800

    feat: billboard

--- src/7.point.vue
+++ src/7.point.vue
@@ -11,31 +11,30 @@ onMounted(() => {
 
   const viewer = new Cesium.Viewer("cesiumContainer", {});
 
+  // entity
+  // const point = new Cesium.Entity({
+  //   position: Cesium.Cartesian3.fromDegrees(120, 30,2000),
+  //   point: {
+  //     pixelSize: 20, // 像素点大小
+  //     color: Cesium.Color.BLUE
+  //   },
+  // });
+  // viewer.entities.add(point)
+
+
   const point = viewer.entities.add({
     id: "point",
-    position: Cesium.Cartesian3.fromDegrees(120, 30, 2000),
+    position: Cesium.Cartesian3.fromDegrees(120,30, 2000),
     point: {
       pixelSize: 20,
       color: Cesium.Color.BLUE,
     },
   });
 
-  const billboard = viewer.entities.add({
-    position: Cesium.Cartesian3.fromDegrees(116, 40, 10),
-    // billboard: {
-    //   image: "/src/assets/0760.png_300.png",
-    //   scale: 0.2,
-    //   color: Cesium.Color.RED,
-    // },
-    label: {
-      text: "label",
-      showBackground:true,
-      backgroundColor: new Cesium.Color(255, 255, 0),
-    },
-  });
+  viewer.zoomTo(point)
 
-  viewer.zoomTo(billboard);
 });
+
 </script>
 
 <style scoped></style>

39ebe8ce3d14fcda7b36ac56868e894870d38c50
insket <2694183181@qq.com>
2023-09-23 21:13:17 +0800

    feat: 组合实体

---
+++ src/9.实体.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="cesiumContainer"></div>
+</template>
+
+<script setup>
+import * as Cesium from "cesium";
+import { onMounted, version } from "vue";
+
+onMounted(() => {
+  Cesium.Ion.defaultAccessToken = import.meta.env.VITE_APP_ION_KEY;
+
+  const viewer = new Cesium.Viewer("cesiumContainer", {
+    infoBox: false, // 点击提示的信息
+    selectionIndicator: false, // 点击选中 的状态框
+  });
+
+  // 线
+  const polyline = viewer.entities.add({
+    polyline: {
+      //  返回笛卡尔坐标数组
+      positions: Cesium.Cartesian3.fromDegreesArray([120, 20, 121, 20, 121, 21]),
+      width: 10,
+      material: Cesium.Color.RED,
+    },
+  });
+
+  // 多边形
+  const polygon = viewer.entities.add({
+    polygon: {
+      hierarchy: {
+        positions: Cesium.Cartesian3.fromDegreesArray([120, 25, 121, 25, 121, 25.5, 120, 25.5]),
+      },
+      material: Cesium.Color.RED,
+      height: 0,
+      extrudedHeight: 50000,
+      outline: true,
+      outlineColor: Cesium.Color.BLUE,
+      fill: false,
+    },
+  });
+
+  // 盒子
+  const box = viewer.entities.add({
+    position: Cesium.Cartesian3.fromDegrees(119, 30, 2000),
+    box: {
+      dimensions: new Cesium.Cartesian3(2000, 1000, 3000),
+      material: Cesium.Color.PINK,
+    },
+  });
+
+  // 椭圆
+  const ellipse = viewer.entities.add({
+    position: Cesium.Cartesian3.fromDegrees(118, 30, 2000),
+    ellipse: {
+      semiMajorAxis: 200,
+      semiMinorAxis: 100,
+      material: Cesium.Color.VIOLET,
+      rotation: Math.PI / 4,
+    },
+  });
+
+  // 矩形
+  const rectangle = viewer.entities.add({
+    rectangle: {
+      coordinates: Cesium.Rectangle.fromDegrees(120,40,123,45),
+      extrudedHeight:200000,
+      material: "/src/assets/0760.png_300.png"
+    },
+  });
+
+  viewer.zoomTo(rectangle);
+});
+</script>
+
+<style scoped></style>
--- src/App.vue
+++ src/App.vue
@@ -11,61 +11,27 @@ onMounted(() => {
 
   const viewer = new Cesium.Viewer("cesiumContainer", {});
 
-  // 线
-  const polyline = viewer.entities.add({
+  const billoboard = viewer.entities.add({
+    position: Cesium.Cartesian3.fromDegrees(120, 30, 100),
+    billboard: {
+      image: "/src/assets/touno.png",
+      scale: 0.9,
+      color: Cesium.Color.WHITE,
+      transparent: true,
+    },
     polyline: {
-      //  返回笛卡尔坐标数组
-      positions: Cesium.Cartesian3.fromDegreesArray([120, 20, 121, 20, 121, 21]),
-      width: 10,
-      material: Cesium.Color.RED,
+      positions: Cesium.Cartesian3.fromDegreesArrayHeights([120, 30, 0, 120, 30, 100]),
+      material: Cesium.Color.AQUA,
+    },
+    position: Cesium.Cartesian3.fromDegrees(120, 30, 100),
+    label: {
+      text: "xxx小区",
+      fillColor: Cesium.Color.AZURE,
+      pixelOffset: new Cesium.Cartesian2(0, -60),
     },
   });
 
-  // 多边形
-  const polygon = viewer.entities.add({
-    polygon: {
-      hierarchy: {
-        positions: Cesium.Cartesian3.fromDegreesArray([120, 25, 121, 25, 121, 25.5, 120, 25.5]),
-      },
-      material: Cesium.Color.RED,
-      height: 0,
-      extrudedHeight: 50000,
-      outline: true,
-      outlineColor: Cesium.Color.BLUE,
-      fill: false,
-    },
-  });
-
-  // 盒子
-  const box = viewer.entities.add({
-    position: Cesium.Cartesian3.fromDegrees(119, 30, 2000),
-    box: {
-      dimensions: new Cesium.Cartesian3(2000, 1000, 3000),
-      material: Cesium.Color.PINK,
-    },
-  });
-
-  // 椭圆
-  const ellipse = viewer.entities.add({
-    position: Cesium.Cartesian3.fromDegrees(118, 30, 2000),
-    ellipse: {
-      semiMajorAxis: 200,
-      semiMinorAxis: 100,
-      material: Cesium.Color.VIOLET,
-      rotation: Math.PI / 4,
-    },
-  });
-
-  // 矩形
-  const rectangle = viewer.entities.add({
-    rectangle: {
-      coordinates: Cesium.Rectangle.fromDegrees(120,40,123,45),
-      extrudedHeight:200000,
-      material: "/src/assets/0760.png_300.png"
-    },
-  });
-
-  viewer.zoomTo(rectangle);
+  viewer.zoomTo(billoboard);
 });
 </script>
 
